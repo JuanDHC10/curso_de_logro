5e8a865752e46eba3001f7703828003c7bd3ccf4
JuanDHC10 <juanhidalgo1017@gmail.com>
2025-03-10 11:14:17 -0400

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,6 @@
+nombre = "Juan"
+edad = 19
+ciudad = "Maracaibo"
+print(f"El se llama: ", nombre)
+print(f"La edad es: ", edad)
+print(f"La ciudad es: ", ciudad)
---
+++ ejercicio2.py
@@ -0,0 +1,5 @@
+programacion = 10
+logica = 8
+diseño = 7
+notas = programacion + logica + diseño / 3
+print(f"La nota definitiva es:", notas)

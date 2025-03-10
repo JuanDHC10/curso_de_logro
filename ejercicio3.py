ef516d5319f507206c10a1f7261ead09d66ddca7
JuanDHC10 <juanhidalgo1017@gmail.com>
2025-03-10 11:59:49 -0400

    Add files via upload

---
+++ ejercicio3.py
@@ -0,0 +1,11 @@
+num = int(input("Ingrese el primer numero"))
+num2 = int(input("Ingrese el segundo numero"))
+
+suma = num + num2
+restar = num - num2
+multiplicacion = num * num2
+division = num / num2
+print(F"La suma es:", suma)
+print(F"La resta es:", restar)
+print(F"La multiplicacion es:", multiplicacion)
+print(F"La division es:", division)

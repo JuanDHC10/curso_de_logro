92a33f4f3e2a584f8fdcada93aa8858db0967d71
JuanDHC10 <juanhidalgo1017@gmail.com>
2025-03-12 21:15:37 -0400

    Add files via upload

---
+++ ejercicio5.py
@@ -0,0 +1,9 @@
+num = int(input("ingrese un numero: "))
+
+print("")
+
+resultado = num >= 10 and num <= 20
+
+print(f"Si el numero que ingresaste esta en el rango de 10-20 saldra true, de lo contrario saldra false: ", resultado)
+
+print("")
